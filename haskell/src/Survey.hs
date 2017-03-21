034c9b6ebd27686862293d3b628da3c6758e1cca
Arnaud Bailly <arnaud.oqube@gmail.com>
2017-03-21 10:14:40 +0100

    add explicit response type

--- haskell/src/Survey.hs
+++ haskell/src/Survey.hs
@@ -9,15 +9,20 @@ data Survey = Survey { previousQuestions :: [ Question ]
                      }
               deriving (Show)
 
+data Response = Graded Int
+              | Option Int
+              | FreeText Text
+              deriving  (Show)
+
 data Question = QCM { question   :: Text
                     , qcmOptions :: [ Text ]
-                    , response   :: Maybe Int
+                    , response   :: Maybe Response
                     }
               | Grade { question   :: Text
                       , gradeRange :: (Int, Int)
-                      , response   :: Maybe Int
+                      , response   :: Maybe Response
                       }
               | OpenQuestion { question :: Text
-                             , response :: Maybe Text
+                             , response :: Maybe Response
                              }
               deriving (Show)

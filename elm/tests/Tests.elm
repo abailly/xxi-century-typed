261325b81f9e0067fecbc719375f2ef804de10b3
Arnaud Bailly <arnaud.oqube@gmail.com>
2017-03-27 12:24:49 +0200

    refactor tests to factorize assertion

--- elm/tests/Tests.elm
+++ elm/tests/Tests.elm
@@ -7,30 +7,25 @@ import String
 import Quizz exposing (..)
 
 
+assertResponseIs : Question -> ResponseStatus -> (() -> Expect.Expectation)
+assertResponseIs question expectedStatus =
+    \() ->
+        let
+            actual =
+                checkResponseVsExpectation question
+        in
+            Expect.equal actual expectedStatus
+
+
 all : Test
 all =
     describe "Holy Grail Test Suite"
         [ describe "Checking Answer"
             [ test "yields Unknown given no response" <|
-                \() ->
-                    let
-                        actual =
-                            checkResponseVsExpectation (Question "Foo?" "bar" Nothing)
-                    in
-                        Expect.equal actual Unknown
+                assertResponseIs (Question "Foo?" "bar" Nothing) Unknown
             , test "yields Correct given response matches expected" <|
-                \() ->
-                    let
-                        actual =
-                            checkResponseVsExpectation (Question "Foo?" "bar" (Just "bar"))
-                    in
-                        Expect.equal actual Correct
+                assertResponseIs (Question "Foo?" "bar" (Just "bar")) Correct
             , test "yields Incorrect given response does not match expected" <|
-                \() ->
-                    let
-                        actual =
-                            checkResponseVsExpectation (Question "Foo?" "bar" (Just "baz"))
-                    in
-                        Expect.equal actual Incorrect
+                assertResponseIs (Question "Foo?" "bar" (Just "baz")) Incorrect
             ]
         ]

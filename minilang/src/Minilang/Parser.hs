05e2ed2753faf9f8e02a1eefe7a66cc94237fff3
Arnaud Bailly <arnaud.oqube@gmail.com>
2018-02-11 18:33:42 +0100

    introduce pairs, declarations, unit

--- minilang/src/Minilang/Parser.hs
+++ minilang/src/Minilang/Parser.hs
@@ -8,13 +8,17 @@ import           Text.Parsec
 import           Text.Parsec.Language  (haskellDef)
 import qualified Text.Parsec.Token     as Tokens
 
-data AST = U
+data AST = U | Unit
          | I Integer
          | D Double
          | Var Text
          | Ap AST AST
          | Abs Binding AST
          | Pi Binding AST AST
+         | Sigma Binding AST AST
+         | P1 AST | P2 AST
+         | Pair AST AST
+         | Decl Binding AST AST
          | Err Text
   deriving (Eq, Show, Read, Generic)
 
@@ -27,38 +31,71 @@ data Binding = B Text
 -- Reads a /MiniLang/ expression and returns its AST.
 parseML
   :: Text -> AST
-parseML input =
+parseML =
+  doParse program
+
+parseMLExpr
+  :: Text -> AST
+parseMLExpr =
+  doParse expr
+
+doParse
+  :: MLParser AST -> Text -> AST
+doParse parser input =
   either (Err . pack . show) id $ runParser parser () "" (unpack input)
 
 -- * Parser
 
 type MLParser a = Parsec String () a
 
-parser
+program
   :: MLParser AST
-parser = dependent_product
-     <|> abstraction
-     <|> try application
-     <|> term
+program = decl
 
-dependent_product
+decl
   :: MLParser AST
-dependent_product = pi >> spaces >> (Pi <$> binding <*> (colon *> parser) <*> (dot *> parser))
+decl = Decl <$> binding <*> (colon *> expr) <*> (equal *> expr)
 
-abstraction
-  :: MLParser AST
-abstraction = lambda >> spaces >> (Abs <$> binding <*> (dot *> parser))
-
-
-application
-  :: MLParser AST
-application = Ap <$> term <*> (spaces *> parser)
+expr
+    :: MLParser AST
+expr = dependent_product
+   <|> dependent_sum
+   <|> abstraction
+   <|> projection
+   <|> try pair
+   <|> try application
+   <|> term
 
 term
   :: MLParser AST
-term = universe
-   <|> number
+term = number
+   <|> unit
    <|> identifier
+
+dependent_product
+  :: MLParser AST
+dependent_product = pi >> spaces >> (Pi <$> binding <*> (colon *> expr) <*> (dot *> expr))
+
+dependent_sum
+  :: MLParser AST
+dependent_sum = sigma >> spaces >> (Sigma <$> binding <*> (colon *> expr) <*> (dot *> expr))
+
+abstraction
+  :: MLParser AST
+abstraction = lambda >> spaces >> (Abs <$> binding <*> (dot *> expr))
+
+projection
+  :: MLParser AST
+projection = try (P1 <$> (pi1 >> dot *> expr))
+         <|> P2 <$> (pi2 >> dot *> expr)
+
+pair
+  :: MLParser AST
+pair = lpar *> (Pair <$> expr <*> (comma *> expr)) <* rpar
+
+application
+  :: MLParser AST
+application = Ap <$> term <*> (spaces *> expr)
 
 -- * Lexer
 
@@ -66,19 +103,31 @@ lexer
   :: Tokens.GenTokenParser String () Identity
 lexer = Tokens.makeTokenParser haskellDef
 
-number, identifier, universe
+number, identifier, unit
   :: MLParser AST
 
 number = either I D <$> Tokens.naturalOrFloat lexer
-identifier = Var . pack <$> Tokens.identifier lexer
-universe = string "U" *> pure U
+identifier = do
+  s <- Tokens.identifier lexer
+  case s of
+    "U"   -> pure U
+    other -> pure $ Var (pack other)
+unit   = string "()" *> pure Unit
 
-lambda, dot, colon, pi
+lambda, dot, colon, pi, sigma, equal, pi1, pi2, comma
+  ,lpar, rpar
   :: MLParser ()
 lambda = char 'λ' >> pure ()
-dot = spaces >> char '.' >> spaces >> pure ()
-colon = spaces >> char ':' >> spaces >> pure ()
-pi = char 'Π' >> pure ()
+dot    = spaces >> char '.' >> spaces >> pure ()
+colon  = spaces >> char ':' >> spaces >> pure ()
+comma  = spaces >> char ',' >> spaces >> pure ()
+lpar   = char '(' >> spaces >> pure ()
+rpar   = spaces >> char ')' >> pure ()
+pi     = char 'Π' >> pure ()
+pi1    = string "π1" >> pure ()
+pi2    = string "π2" >> pure ()
+sigma  = char 'Σ' >> pure ()
+equal  = char '=' >> spaces >> pure ()
 
 binding
   :: MLParser Binding
--- minilang/test/Minilang/ParserSpec.hs
+++ minilang/test/Minilang/ParserSpec.hs
@@ -6,24 +6,53 @@ import           Test.Hspec
 spec :: Spec
 spec = describe "Minilang Core" $ do
 
-  it "parse Number" $ do
-    parseML "12" `shouldBe` I 12
-    parseML "12.4" `shouldBe` D 12.4
+  describe "Parsing Terms and Expressions" $ do
 
-  it "parse Variable" $ do
-    parseML "abc" `shouldBe` Var "abc"
+    it "parse Number" $ do
+      parseMLExpr "12" `shouldBe` I 12
+      parseMLExpr "12.4" `shouldBe` D 12.4
 
-  it "parse Universe" $ do
-    parseML "U" `shouldBe` U
+    it "parse Variable" $ do
+      parseMLExpr "abc" `shouldBe` Var "abc"
 
-  it "parse Application" $ do
-    parseML "abc fge" `shouldBe` Ap (Var "abc") (Var "fge")
-    parseML "abc fge 12" `shouldBe` Ap (Var "abc") (Ap (Var "fge") (I 12))
+    it "parse Universe" $ do
+      parseMLExpr "U" `shouldBe` U
+      parseMLExpr "Uabc" `shouldBe` Var "Uabc"
 
-  it "parse Abstraction" $ do
-    parseML "λ abc . abc" `shouldBe` Abs (B "abc") (Var "abc")
-    parseML "λ abc . abc ghe" `shouldBe` Abs (B "abc") (Ap (Var "abc") (Var "ghe"))
-    parseML "λ _ . abc" `shouldBe` Abs Wildcard (Var "abc")
+    it "parse Universe" $ do
+      parseMLExpr "U" `shouldBe` U
 
-  it "parse Dependent Product" $ do
-    parseML "Π abc : U . abc" `shouldBe` Pi (B "abc") U (Var "abc")
+    it "parse Unit" $ do
+      parseMLExpr "()" `shouldBe` Unit
+
+    it "parse Application" $ do
+      parseMLExpr "abc fge" `shouldBe` Ap (Var "abc") (Var "fge")
+      parseMLExpr "abc fge 12" `shouldBe` Ap (Var "abc") (Ap (Var "fge") (I 12))
+
+    it "parse Abstraction" $ do
+      parseMLExpr "λ abc . abc" `shouldBe` Abs (B "abc") (Var "abc")
+      parseMLExpr "λ abc . abc ghe" `shouldBe` Abs (B "abc") (Ap (Var "abc") (Var "ghe"))
+      parseMLExpr "λ _ . abc" `shouldBe` Abs Wildcard (Var "abc")
+
+    it "parse Dependent Product" $ do
+      parseMLExpr "Π abc : U . abc" `shouldBe` Pi (B "abc") U (Var "abc")
+      parseMLExpr "Πabc:U.abc" `shouldBe` Pi (B "abc") U (Var "abc")
+
+    it "parse Dependent Sum" $ do
+      parseMLExpr "Σ abc : U . abc" `shouldBe` Sigma (B "abc") U (Var "abc")
+      parseMLExpr "Σabc:   U. abc" `shouldBe` Sigma (B "abc") U (Var "abc")
+
+    it "parse Projections" $ do
+      parseMLExpr "π1.abc" `shouldBe` P1 (Var "abc")
+      parseMLExpr "π2.abc" `shouldBe` P2 (Var "abc")
+
+    it "parse Pairing" $ do
+      parseMLExpr "(abc,12)" `shouldBe` Pair (Var "abc") (I 12)
+
+  describe "Declarations" $ do
+
+    it "generic id function" $ do
+      parseML "id : Π A : U . Π _ : A . A = λ A . λ x . x"
+        `shouldBe` Decl (B "id")
+                   (Pi (B "A") U (Pi Wildcard (Var "A") (Var "A")))
+                   (Abs (B "A") (Abs (B "x") (Var "x")))

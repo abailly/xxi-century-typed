d5ecb75309635a5693c9e908ad5d07d8e77f6d27
Arnaud Bailly <arnaud.oqube@gmail.com>
2018-02-11 13:31:19 +0100

    introduce dependent product

--- minilang/src/Minilang/Parser.hs
+++ minilang/src/Minilang/Parser.hs
@@ -3,16 +3,25 @@ module Minilang.Parser where
 import           Data.Functor.Identity (Identity)
 import           Data.Text             (Text, pack, unpack)
 import           GHC.Generics          (Generic)
+import           Prelude               hiding (pi)
 import           Text.Parsec
 import           Text.Parsec.Language  (haskellDef)
 import qualified Text.Parsec.Token     as Tokens
 
-data AST = I Integer
+data AST = U
+         | I Integer
          | D Double
          | Var Text
          | Ap AST AST
+         | Abs Binding AST
+         | Pi Binding AST AST
          | Err Text
   deriving (Eq, Show, Read, Generic)
+
+data Binding = B Text
+             | Wildcard
+  deriving (Eq, Show, Read, Generic)
+
 
 -- | Top-level parser for MiniLang.
 -- Reads a /MiniLang/ expression and returns its AST.
@@ -27,8 +36,19 @@ type MLParser a = Parsec String () a
 
 parser
   :: MLParser AST
-parser = try application
+parser = dependent_product
+     <|> abstraction
+     <|> try application
      <|> term
+
+dependent_product
+  :: MLParser AST
+dependent_product = pi >> spaces >> (Pi <$> binding <*> (colon *> parser) <*> (dot *> parser))
+
+abstraction
+  :: MLParser AST
+abstraction = lambda >> spaces >> (Abs <$> binding <*> (dot *> parser))
+
 
 application
   :: MLParser AST
@@ -36,7 +56,8 @@ application = Ap <$> term <*> (spaces *> parser)
 
 term
   :: MLParser AST
-term = number
+term = universe
+   <|> number
    <|> identifier
 
 -- * Lexer
@@ -45,8 +66,21 @@ lexer
   :: Tokens.GenTokenParser String () Identity
 lexer = Tokens.makeTokenParser haskellDef
 
-number, identifier
+number, identifier, universe
   :: MLParser AST
 
 number = either I D <$> Tokens.naturalOrFloat lexer
 identifier = Var . pack <$> Tokens.identifier lexer
+universe = string "U" *> pure U
+
+lambda, dot, colon, pi
+  :: MLParser ()
+lambda = char 'λ' >> pure ()
+dot = spaces >> char '.' >> spaces >> pure ()
+colon = spaces >> char ':' >> spaces >> pure ()
+pi = char 'Π' >> pure ()
+
+binding
+  :: MLParser Binding
+binding = string "_" *> pure Wildcard
+  <|> B . pack <$> Tokens.identifier lexer
--- minilang/test/Minilang/ParserSpec.hs
+++ minilang/test/Minilang/ParserSpec.hs
@@ -13,6 +13,17 @@ spec = describe "Minilang Core" $ do
   it "parse Variable" $ do
     parseML "abc" `shouldBe` Var "abc"
 
+  it "parse Universe" $ do
+    parseML "U" `shouldBe` U
+
   it "parse Application" $ do
     parseML "abc fge" `shouldBe` Ap (Var "abc") (Var "fge")
     parseML "abc fge 12" `shouldBe` Ap (Var "abc") (Ap (Var "fge") (I 12))
+
+  it "parse Abstraction" $ do
+    parseML "λ abc . abc" `shouldBe` Abs (B "abc") (Var "abc")
+    parseML "λ abc . abc ghe" `shouldBe` Abs (B "abc") (Ap (Var "abc") (Var "ghe"))
+    parseML "λ _ . abc" `shouldBe` Abs Wildcard (Var "abc")
+
+  it "parse Dependent Product" $ do
+    parseML "Π abc : U . abc" `shouldBe` Pi (B "abc") U (Var "abc")

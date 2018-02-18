1bf5635ec490b626740a3c4a36d1fb6cacb40a24
Arnaud Bailly <arnaud.oqube@gmail.com>
2018-02-18 18:42:15 +0100

    add evaluating asbtractions

--- minilang/src/Minilang/Eval.hs
+++ minilang/src/Minilang/Eval.hs
@@ -10,8 +10,23 @@ newtype Env = Env { unEnv :: Map.Map Name Value }
   deriving (Eq, Show)
 
 data Value = EI Integer | ED Double
-  deriving (Eq, Show, Read)
+           | EU | EUnit
+           | EPair Value Value
+           | EAbs FunClos
+  deriving (Eq, Show)
+
+data FunClos = Cl Binding AST Env
+  deriving (Eq, Show)
 
 eval
-  :: AST -> Env -> (Value, Env)
-eval (I n) env = (EI n, env)
+  :: AST -> Env -> Value
+eval (I n)      _env = EI n
+eval (D d)      _env = ED d
+eval U          _env = EU
+eval Unit       _env = EUnit
+eval (Pair a b)  env = EPair v w
+  where
+    v = eval a env
+    w = eval b env
+eval (Abs p e)  env  = EAbs $ Cl p e env
+eval _ _       = undefined
--- minilang/test/Minilang/EvalSpec.hs
+++ minilang/test/Minilang/EvalSpec.hs
@@ -7,7 +7,17 @@ import           Test.Hspec
 spec :: Spec
 spec = parallel $ describe "Expressions Evaluator" $ do
 
-  it "evaluates a number as itself" $ do
-    let emptyEnv = Env mempty
-    eval (I 12) emptyEnv `shouldBe` (EI 12, emptyEnv)
-    eval (D 12) emptyEnv `shouldBe` (ED 12, emptyEnv)
+  let emptyEnv = Env mempty
+
+  it "evaluates constants to themselves" $ do
+    eval (I 12) emptyEnv `shouldBe` EI 12
+    eval (D 12) emptyEnv `shouldBe` ED 12
+    eval U      emptyEnv `shouldBe` EU
+    eval Unit   emptyEnv `shouldBe` EUnit
+
+  it "evaluates pairs to pairs of values" $ do
+    eval (Pair (D 12) Unit) emptyEnv `shouldBe` EPair (ED 12) EUnit
+
+  it "evaluates abstraction to a closure" $ do
+    eval (Abs (B "foo") (Var "foo")) emptyEnv
+      `shouldBe` EAbs (Cl (B "foo") (Var "foo") emptyEnv)

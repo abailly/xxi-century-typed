7707397f125a6cc9a69a3a9c1cc6b8990834845f
Arnaud Bailly <arnaud.oqube@gmail.com>
2018-02-18 22:02:25 +0100

    handle basic application

--- minilang/src/Minilang/Eval.hs
+++ minilang/src/Minilang/Eval.hs
@@ -1,18 +1,23 @@
 module Minilang.Eval where
 
-import qualified Data.Map        as Map
 import           Data.Text       (Text)
 import           Minilang.Parser
 
 type Name = Text
 
-newtype Env = Env { unEnv :: Map.Map Name Value }
+data Env = EmptyEnv
+         | ExtendPat Env Binding Value
   deriving (Eq, Show)
+
+emptyEnv :: Env
+emptyEnv = EmptyEnv
 
 data Value = EI Integer | ED Double
            | EU | EUnit
            | EPair Value Value
            | EAbs FunClos
+           | EPi Value FunClos
+           | ESig Value FunClos
   deriving (Eq, Show)
 
 data FunClos = Cl Binding AST Env
@@ -20,13 +25,36 @@ data FunClos = Cl Binding AST Env
 
 eval
   :: AST -> Env -> Value
-eval (I n)      _env = EI n
-eval (D d)      _env = ED d
-eval U          _env = EU
-eval Unit       _env = EUnit
-eval (Pair a b)  env = EPair v w
-  where
-    v = eval a env
-    w = eval b env
-eval (Abs p e)  env  = EAbs $ Cl p e env
-eval _ _       = undefined
+eval (I n)      _ = EI n
+eval (D d)      _ = ED d
+eval U          _ = EU
+eval Unit       _ = EUnit
+eval (Pair a b)  ρ = EPair (eval a ρ) (eval b ρ)
+eval (Abs p e)  ρ  = EAbs $ Cl p e ρ
+eval (Pi p t e) ρ  = EPi (eval t ρ) $ Cl p e ρ
+eval (Sigma p t e) ρ = ESig (eval t ρ) $ Cl p e ρ
+eval (Ap u v)      ρ = app (eval u ρ) (eval v ρ)
+eval (Var x) ρ = rho ρ x
+eval e ρ       = error $ "don't know how to evaluate " ++ show e ++ " in  " ++ show ρ
+
+
+app :: Value -> Value -> Value
+app (EAbs f@Cl{}) v = inst f v
+app _ _             = undefined
+
+inst :: FunClos -> Value -> Value
+inst (Cl b e ρ) v = eval e (ExtendPat ρ b v)
+
+rho :: Env -> Name -> Value
+rho EmptyEnv x = error $ "name " ++ show x ++ " is not defined in empty environment"
+rho (ExtendPat ρ b v) x
+  | x `inPat` b = proj x b v
+  | otherwise   = rho ρ x
+
+inPat :: Name -> Binding -> Bool
+inPat x (B p') | x == p' = True
+inPat _ _      = undefined
+
+proj :: Name -> Binding -> Value -> Value
+proj _ (B _) v = v
+proj _ _ _     = undefined
--- minilang/test/Minilang/EvalSpec.hs
+++ minilang/test/Minilang/EvalSpec.hs
@@ -7,8 +7,6 @@ import           Test.Hspec
 spec :: Spec
 spec = parallel $ describe "Expressions Evaluator" $ do
 
-  let emptyEnv = Env mempty
-
   it "evaluates constants to themselves" $ do
     eval (I 12) emptyEnv `shouldBe` EI 12
     eval (D 12) emptyEnv `shouldBe` ED 12
@@ -21,3 +19,15 @@ spec = parallel $ describe "Expressions Evaluator" $ do
   it "evaluates abstraction to a closure" $ do
     eval (Abs (B "foo") (Var "foo")) emptyEnv
       `shouldBe` EAbs (Cl (B "foo") (Var "foo") emptyEnv)
+
+  it "evaluates product type" $ do
+    eval (Pi (B "foo") U (Var "bar")) emptyEnv
+      `shouldBe` EPi EU (Cl (B "foo") (Var "bar") emptyEnv)
+
+  it "evaluates sum type" $ do
+    eval (Sigma (B "foo") U (Var "bar")) emptyEnv
+      `shouldBe` ESig EU (Cl (B "foo") (Var "bar") emptyEnv)
+
+  it "evaluates Application of a function" $ do
+    eval (Ap (Abs (B "x") (Var "x")) (I 12)) emptyEnv
+      `shouldBe` EI 12

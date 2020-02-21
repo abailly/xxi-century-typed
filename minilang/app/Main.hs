f8f9e0dd33157a4f2e79846056e4663290d845ce
Arnaud Bailly <arnaud.oqube@gmail.com>
2020-02-21 17:17:09 +0100

    working

--- minilang/app/Main.hs
+++ minilang/app/Main.hs
@@ -2,9 +2,46 @@ module Main where
 
 import           Minilang.IO
 import           Minilang.REPL
+import           Options.Applicative
 import           System.IO             (stdin, stdout)
 import           System.Posix.IO       (stdInput)
 import           System.Posix.Terminal (queryTerminal)
+
+data RunMode = Local
+    | Server
+    deriving (Eq, Show)
+
+data Options = Options
+    { serverPort :: Int
+    , runMode    :: RunMode
+    }
+    deriving (Eq, Show)
+
+optionsParser :: ParserInfo Options
+optionsParser =
+  info (helper <*> minilangOptions)
+  ( header "Minilang"
+    <> fullDesc
+  )
+
+minilangOptions :: Parser Options
+minilangOptions =
+  Options <$> portOption <*> runModeOption
+
+portOption :: Parser Int
+portOption =
+  option auto (long "port"
+               <> short 'p'
+               <> value 7890
+               <> metavar "INT"
+               <> help "port to listent on (default: 7890)"
+              )
+
+runModeOption :: Parser RunMode
+runModeOption =
+  flag Local Server (long "server"
+                     <> short 'S'
+                     <> help "run in server mode, listening for client REPLs over HTTP")
 
 main :: IO ()
 main = do

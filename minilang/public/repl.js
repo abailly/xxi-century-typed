dad568450b7f7c91ba5d3ef46af0092cc8a8fbfb
Arnaud Bailly <arnaud.oqube@gmail.com>
2020-02-22 09:10:10 +0100

    scroll output window when more content is available

--- minilang/public/repl.js
+++ minilang/public/repl.js
@@ -5,24 +5,29 @@ var websocket = (function() {
     return ws;
 })();
 
+var commandHistory = [];
+
 websocket.onopen = function(e) {
     document.getElementById("command").classList.add("connected");
 };
 
-function appendMessage(reader) {
+function appendOutput(text) {
+    const root = document.getElementById("minilang");
+    const sub = document.createElement("div");
+    sub.classList.add("output");
+    sub.appendChild(text);
+    root.appendChild(sub);
+};
+
+function readMessageAndAppend(reader) {
     return () => {
-        const root = document.getElementById("minilang");
-        const sub = document.createElement("div");
-        sub.classList.add("output");
-        const txt = document.createTextNode(JSON.stringify(reader.result));
-        sub.appendChild(txt);
-        root.appendChild(sub);
+        appendOutput(document.createTextNode(JSON.stringify(reader.result)));
     };
-}
+};
 
 websocket.onmessage = function(e) {
     const reader = new FileReader();
-    reader.addEventListener('loadend', appendMessage(reader));
+    reader.addEventListener('loadend', readMessageAndAppend(reader));
 
     if(e.data instanceof ArrayBuffer) {
         reader.readAsText(new Blob([e.data]));
@@ -34,17 +39,27 @@ websocket.onmessage = function(e) {
 function makeCommand(val) {
     if(val === ":env") {
         return {"tag":"Com","contents":{"tag":"DumpEnv"}};
+    } else if(val === ":clear") {
+        return {"tag":"Com","contents":{"tag":"ClearEnv"}};
     } else {
         return {"tag":"In","contents":val};
     }
 };
 
 function sendCommand(e) {
+    const input = document.getElementById("command");
     if (e.keyCode == 13) {
-        const command = makeCommand(document.getElementById("command").value);
+        const command = makeCommand(input.value);
+        appendOutput(document.createTextNode("> "+ input.value));
+        commandHistory.push(input.value);
         websocket.send(JSON.stringify(command));
+        input.value = "";
+        return false;
+    } else if(e.keyCode == 38) {
+        input.value = commandHistory.pop() || "";
         return false;
     }
+
     return true;
 };
 

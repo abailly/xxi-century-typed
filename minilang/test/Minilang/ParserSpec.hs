34a599c9cad6e7da2e0956c35ca200062e470b71
Arnaud Bailly <arnaud.oqube@gmail.com>
2020-03-11 12:52:28 +0100

    extract tests for pretty-printer to own module

--- minilang/test/Minilang/ParserSpec.hs
+++ minilang/test/Minilang/ParserSpec.hs
@@ -3,9 +3,7 @@ module Minilang.ParserSpec where
 import           Data.Monoid       ((<>))
 import qualified Data.Text         as Text
 import           Minilang.Parser
-import           Minilang.Pretty
 import           Test.Hspec
-import           Test.QuickCheck   as QC
 import           Text.Parsec.Error
 import           Text.Parsec.Pos
 
@@ -241,27 +239,6 @@ spec = parallel $ describe "Minilang Parser" $ do
 
         `shouldBe` Let (RDecl (B "NEList") (Pi (B "A") U U) (Abs (B "A") (Sum [Choice "S" (Just (Var "A")),Choice "C" (Just (Sigma (B "a") (Var "A") (Ap (Var "NEList") (Var "A"))))]))) (Let (Decl (B "elimNEList") (Pi (B "A") U (Pi (B "C") (Pi Wildcard (Ap (Var "NEList") (Var "A")) U) (Pi Wildcard (Pi (B "a") (Var "A") (Ap (Var "C") (Ctor "S" (Just (Var "a"))))) (Pi Wildcard (Pi (B "a") (Sigma Wildcard (Var "A") (Ap (Var "NEList") (Var "A"))) (Ap (Var "C") (Ctor "C" (Just (Var "a"))))) (Pi (B "b") (Ap (Var "NEList") (Var "A")) (Ap (Var "C") (Var "b"))))))) (Abs (B "A") (Abs (B "C") (Abs (B "h0") (Abs (B "h1") (Case [Clause "S" (Abs (B "a") (Ap (Var "h0") (Var "a"))),Clause "C" (Abs (B "a") (Ap (Var "h1") (Var "a")))])))))) (Let (Decl (B "select") (Pi Wildcard (Ap (Var "NEList") (Var "Bool")) U) (Case [Clause "S" (Abs Wildcard (Var "Unit")),Clause "C" (Abs Wildcard (Var "Unit"))])) Unit))
 
-  describe "Pretty-printing Expressions" $ do
-
-    it "pretty prints simple expressions" $ do
-      show (pretty (parseProgram False "Π abc : U . abc") )
-        `shouldBe` "Π abc : U . abc"
-      show (pretty (parseProgram False "λ abc . π1.(abc,feg)") )
-        `shouldBe` "λ abc . π1.(abc, feg)"
-      show (pretty (parseProgram False "λ (abc, (x,y)) . π1.($true, $false)") )
-        `shouldBe` "λ (abc, (x, y)) . π1.($true, $false)"
-
-    it "pretty prints declarations" $ do
-      show (pretty (parseProgram False "let id : Π A : U . Π _ : A . A = λ A . λ x . x ;\n()") )
-        `shouldBe` "let id : Π A : U . A → A = λ A . λ x . x ;\n()"
-      show (pretty (parseProgram False "let rec Nat : U = Sum (zero | succ Nat) ;\nlet id : Π A : U . Π _ : A . A = λ A . λ x . x; ()"))
-        `shouldBe` "let rec Nat : U = Sum(zero| succ Nat) ;\nlet id : Π A : U . A → A = λ A . λ x . x ;\n()"
-      show (pretty (parseProgram False "let rec natrec : Π C : Nat → U . C $zero → (Π n : Nat.C n → C ($succ n)) → Π n : Nat . C n = λ C . λ a . λ g . case (zero → a | succ n1 → (g n1) (natrec C a g n1)); ()"))
-        `shouldBe` "let rec natrec : Π C : Nat → U . (C $zero) → Π n : Nat . (C n) → (C ($succ n)) → Π n : Nat . (C n) = λ C . λ a . λ g . case(zero → λ _ . a| succ → λ n1 . ((g n1) ((((natrec C) a) g) n1))) ;\n()"
-
-    it "is inverse to parsing" $ property $ prop_parsingIsInvertToPrettyPrinter
-    it "has parsing as inverse" $ property $ prop_prettyPrintingIsInverseToParsing
-
   describe "Error handling" $ do
     describe "skipErrorTo" $ do
 
@@ -293,41 +270,3 @@ spec = parallel $ describe "Minilang Parser" $ do
                         (Case [ Clause "tt" errorNode
                               , Clause "ff" (Abs Wildcard Unit)
                               ])) Unit
-
-
-newtype TestAST = T { unAst :: AST }
-  deriving (Eq, Show)
-
-arbitraryAst :: Gen AST
-arbitraryAst = unAst <$> arbitrary
-
-leafAST :: [ (Int, Gen AST) ]
-leafAST = [ (1, pure One)
-          , (1, pure Unit)
-          , (1, I <$> arbitrary)
-          , (1, D <$> arbitrary)
-          ]
-
-genAST :: Int ->  Gen AST
-genAST 0 = frequency leafAST
-genAST n =
-  frequency $ leafAST <> [ (3, Ap <$> genAST (n-1) <*> genAST (n-1)) ]
-
-instance Arbitrary TestAST where
-  arbitrary = do
-    h <- QC.choose (0, 3)
-    T <$> genAST h
-
-prop_parsingIsInvertToPrettyPrinter :: TestAST -> Property
-prop_parsingIsInvertToPrettyPrinter (T ast) =
-  let pp = show (pretty ast)
-      parsed = parseProgram False (Text.pack pp)
-      msg = "from : " <> show ast <> ", pretty: " <> pp <> ", back to: " <> show parsed
-  in collect (height ast) $ counterexample msg $ parsed == ast
-
-prop_prettyPrintingIsInverseToParsing :: TestAST -> Property
-prop_prettyPrintingIsInverseToParsing (T ast) =
-  let pp = show (pretty ast)
-      parsed = parseProgram False (Text.pack pp)
-      msg = "from : " <> show ast <> ", pretty: " <> pp <> ", pretty parsed is: " <> show (pretty parsed)
-  in counterexample msg $ pp == show (pretty parsed)
